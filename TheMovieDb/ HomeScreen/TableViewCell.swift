9857fdcbbbad9f4c724d9765edaea259bc38559a
sara.galal <sara.galal@ibtikar.net.sa>
2019-09-10 09:31:41 +0200

    add search

---
+++ TheMovieDb/ HomeScreen/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TheMovieDb
+//
+//  Created by sara.galal on 9/9/19.
+//  Copyright © 2019 Sara Galal. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
